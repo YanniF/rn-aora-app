1209f67e2d4add4cbd84d126dae83b1c8d2f08c6
Yanni Fraga <yanni_fernanda@hotmail.com>
2024-08-27 20:03:53 +0200

    Index screen

---
+++ react-native-aora-app/components/CustomButton.jsx
@@ -0,0 +1,21 @@
+import {TouchableOpacity, Text} from "react-native";
+
+const CustomButton = ({title, handlePress, isLoading, containerStyles, textStyles}) => {
+  return (
+    <TouchableOpacity
+      className={
+        `bg-secondary rounded-xl min-h-[62px] flex flex-row justify-center items-center ${containerStyles} 
+        ${isLoading ? "opacity-50" : ""
+      }`}
+      onPress={handlePress}
+      activeOpacity={0.7}
+      disabled={isLoading}
+    >
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+export default CustomButton
